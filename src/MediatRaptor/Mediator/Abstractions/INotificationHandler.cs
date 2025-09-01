6755dd8a3d4dd5b9a00a5b3dbaa359ef1b110b1a
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-09-01 08:56:56 +0300

    Add notificationsfor pub/sub events

---
+++ src/MediatRaptor/Mediator/Abstractions/INotificationHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace MediatRaptor.Mediator.Abstractions
+{
+    /// <summary>
+    /// Notification handler interface
+    /// </summary>
+    public interface INotificationHandler<in TNotification>
+        where TNotification : INotification
+    {
+        Task Handle(TNotification notification, CancellationToken cancellationToken);
+    }
+}
