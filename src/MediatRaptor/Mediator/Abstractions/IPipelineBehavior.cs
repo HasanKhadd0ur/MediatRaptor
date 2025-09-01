689474499984c51fafee342b4d4323a288535547
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-09-01 18:43:06 +0300

    Add Pipeline Behavior Abstraction

---
+++ src/MediatRaptor/Mediator/Abstractions/IPipelineBehavior.cs
@@ -0,0 +1,13 @@
+﻿namespace MediatRaptor.Mediator.Abstractions
+{
+    public delegate Task<TResponse> RequestHandlerDelegate<TResponse>();
+
+    /// <summary>
+    /// Pipeline behavior that wraps request handling.
+    /// </summary>
+    public interface IPipelineBehavior<in TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    {
+        Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next);
+    }
+}
