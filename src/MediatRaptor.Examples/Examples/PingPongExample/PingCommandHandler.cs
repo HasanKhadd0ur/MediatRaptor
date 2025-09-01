e9bf3f0e16261832be99edf7af7f0975861a22df
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-09-01 18:44:30 +0300

    Add PingPong Example for Ilustration

---
+++ src/MediatRaptor.Examples/Examples/PingPongExample/PingCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatRaptor.Mediator.Abstractions;
+
+namespace MediatRaptor.Examples.Examples.PingPongExample
+{
+    public class PingCommandHandler : IRequestHandler<PingCommand, string>
+    {
+        private readonly IMediator _mediator;
+
+        public PingCommandHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<string> Handle(PingCommand request, CancellationToken cancellationToken)
+        {
+            var response = $"Pong received your message: {request.Message}";
+
+            return response;
+        }
+    }
+}
--- src/MediatRaptor.Examples/Program.cs
+++ src/MediatRaptor.Examples/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MediatRaptor.Examples.Examples.PingPongExample;
+using MediatRaptor.Mediator.Abstractions;
+using MediatRaptor.Mediator.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+// Setup DI
+var services = new ServiceCollection();
+
+// Register MediatRaptor
+services.AddMediatRaptor(typeof(PingCommand).Assembly);
+
+// Build provider
+var provider = services.BuildServiceProvider();
+
+// Resolve mediator
+var mediator = provider.GetRequiredService<IMediator>();
+
+// Run Ping → Pong example
+Console.WriteLine("Sending PingCommand...");
+var result = await mediator.Send(new PingCommand("Hello from Ping!"));
+
+Console.WriteLine($"[Request Result] {result}");
