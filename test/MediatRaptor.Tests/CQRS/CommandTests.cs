22c4b5487c0ee8678f81b3f5a77bcd3b364d3c06
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-09-10 11:39:28 +0300

    Add Command and Query Tests for MediatRaptor

---
+++ test/MediatRaptor.Tests/CQRS/CommandTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using MediatRaptor.CQRS.Commands;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MediatRaptor.Tests.CQRS
+{
+    public class CommandTests
+    {
+        private record AddCommand(int A, int B) : ICommand<int>;
+        private class AddHandler : ICommandHandler<AddCommand, int>
+        {
+            public Task<int> Handle(AddCommand command, CancellationToken cancellationToken) =>
+                Task.FromResult(command.A + command.B);
+        }
+
+        [Fact]
+        public async Task CommandHandler_Should_Return_Sum()
+        {
+            var handler = new AddHandler();
+            var result = await handler.Handle(new AddCommand(2, 3), default);
+
+            result.Should().Be(5);
+        }
+    }
+}
