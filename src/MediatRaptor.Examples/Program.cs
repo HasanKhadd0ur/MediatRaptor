cc21221cc0be66d2d1848087c4dc3e74262fe3e2
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-09-10 11:44:08 +0300

    Add notification handlers and demo for user registration events

--- src/MediatRaptor.Examples/Program.cs
+++ src/MediatRaptor.Examples/Program.cs
@@ -1,4 +1,5 @@
 ﻿using MediatRaptor.Examples.Examples.PingPongExample;
+using MediatRaptor.Examples.Notifications;
 using MediatRaptor.Mediator.Abstractions;
 using MediatRaptor.Mediator.Core;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,6 +9,7 @@ var services = new ServiceCollection();
 
 // Register MediatRaptor
 services.AddMediatRaptor(typeof(PingCommand).Assembly);
+services.AddMediatRaptor(typeof(NotificationDemo).Assembly);
 
 // Build provider
 var provider = services.BuildServiceProvider();
@@ -20,3 +22,8 @@ Console.WriteLine("Sending PingCommand...");
 var result = await mediator.Send(new PingCommand("Hello from Ping!"));
 
 Console.WriteLine($"[Request Result] {result}");
+
+// Run Notifications Demo 
+var NotificationDemo = new NotificationDemo(mediator);
+
+await NotificationDemo.Run();
